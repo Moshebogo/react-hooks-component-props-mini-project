10ab8f08be8389e8af25e81693142c5819fdf873
Eli Moshe <moshebogo@gmail.com>
2023-01-31 17:05:11 -0500

    lab complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import Article from "./Article"
+
+function ArticleList ({posts}) {
+   const articles = posts.map(post => <Article title={post.title} date={post.date} preview={post.preview} key={post.id} />)
+    return (
+        <main>
+           {articles}
+        </main>
+    )
+}
+
+export default ArticleList
