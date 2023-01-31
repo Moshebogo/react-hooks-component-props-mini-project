5a8762b6332e49786038b5cc262bf25af8170ee8
Eli Moshe <moshebogo@gmail.com>
2023-01-31 17:28:18 -0500

    lab complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,20 @@
-function Article ({title, date="January 1, 1970", preview}) {
+function Article ({title, date="January 1, 1970", preview, minutes}) {
+
+    let minutesEmojis = ""
+    if (minutes < 30) {
+        const emojCount = Math.ceil(minutes / 5)
+        minutesEmojis = "☕️".repeat(emojCount) + `  ${minutes} min read.`
+    } else {
+        const emojCount = Math.ceil(minutes / 10)
+        minutesEmojis = "🍱".repeat(emojCount) + `  ${minutes} min read.`
+    }
+
     return (
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
             <p>{preview}</p>
+            <p>{minutesEmojis}</p>
         </article>
     )
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,7 +1,7 @@
 import Article from "./Article"
 
 function ArticleList ({posts}) {
-   const articles = posts.map(post => <Article title={post.title} date={post.date} preview={post.preview} key={post.id} />)
+   const articles = posts.map(post => <Article title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} key={post.id} />)
     return (
         <main>
            {articles}
